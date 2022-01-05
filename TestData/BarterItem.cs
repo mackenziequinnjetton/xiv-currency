e850936ed53b40dd9f427483bf93011a1d66181a
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2022-01-04 20:43:40 -0700

    Add BarterItem class

---
+++ TestData/BarterItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XIVCurrencyClasses
+{
+    public class BarterItem
+    {
+        public string Name { get; set; }
+
+        public BarterItem(string name)
+        {
+            Name = name;
+        }
+    }
+}
