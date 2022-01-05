3f4f3196ad8f19c1b5bdf623059e60ad83c7bd9d
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2022-01-04 17:54:25 -0700

    Remove API query due to incompatibility with program requirements

--- XIVCurrency/XIVCurrencyForm.cs
+++ XIVCurrency/XIVCurrencyForm.cs
@@ -40,32 +40,11 @@ namespace XIVCurrency
             }
         }
 
-        // Query game API for item information, and display
+        // Search sample game data for item information, and display
         // information in resultListBox
         private async void searchButton_Click(object sender, EventArgs e)
         {
-            // API search URL
-            var apiUrl = 
-                @"https://xivapi.com/search?indexes=Itemlimit=10&string=";
-
-            /*HttpResponseMessage apiRequest =
-                (HttpResponseMessage)
-                await $"{apiUrl}{searchTextBox.Text}".GetAsync();*/
-
-            HttpClient = new HttpClient();
-
-            HttpResponseMessage apiRequest = await HttpClient.GetAsync(
-                $"{apiUrl}{searchTextBox.Text}");
-
-            /*JObject apiResponse = (JObject)JsonConvert.DeserializeObject(
-                apiRequest.Content.ReadAsStringAsync().Result);*/
-
-            JObject apiResponse = JObject.Parse(apiRequest.ToString());
-
-            MessageBox.Show($"{apiResponse}");
-
-            apiRequest.Dispose();
-            apiResponse.Dispose();
+            
         }
     }
 }
