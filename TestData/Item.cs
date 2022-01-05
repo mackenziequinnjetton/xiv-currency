320798c9b544095487c2d35c8021ba3032484411
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2022-01-04 20:33:32 -0700

    Add Item class

---
+++ TestData/Item.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XIVCurrencyClasses
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public Currency? Currency { get; set; }
+        public int? CurrencyCost { get; set; }
+        public BarterItem? BarterItem { get; set; }
+        public int? BarterCost { get; set; }
+
+        public Item(string name, Currency? currency, int? currencyCost,
+            BarterItem? barterItem, int? barterCost)
+        {
+            Name = name;
+            Currency = currency;
+            CurrencyCost = currencyCost;
+            BarterItem = barterItem;
+            BarterCost = barterCost;
+        }
+    }
+}
