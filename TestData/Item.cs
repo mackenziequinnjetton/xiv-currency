b47177d20322cd2a9518a39352e4ad6241095b62
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2022-01-04 20:46:58 -0700

    Remove erroneous attempt to nullify reference types in Item class

--- TestData/Item.cs
+++ TestData/Item.cs
@@ -9,13 +9,13 @@ namespace XIVCurrencyClasses
     public class Item
     {
         public string Name { get; set; }
-        public Currency? Currency { get; set; }
+        public Currency Currency { get; set; }
         public int? CurrencyCost { get; set; }
-        public BarterItem? BarterItem { get; set; }
+        public BarterItem BarterItem { get; set; }
         public int? BarterCost { get; set; }
 
-        public Item(string name, Currency? currency, int? currencyCost,
-            BarterItem? barterItem, int? barterCost)
+        public Item(string name, Currency currency, int? currencyCost,
+            BarterItem barterItem, int? barterCost)
         {
             Name = name;
             Currency = currency;
