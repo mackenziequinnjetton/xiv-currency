83be1c8029f05effd2b534535fe727eacbbbb30a
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2022-01-05 17:25:46 -0700

    Start work on TestData class

--- TestData/TestData.cs
+++ TestData/TestData.cs
@@ -5,6 +5,8 @@ namespace XIVCurrencyClasses
     // Sample game data for testing purposes
     public static class TestData
     {
-
+        public static Location[] Locations { get; set; } =
+            { new Location("Radz-at-Han",
+                {}) }
     }
 }
